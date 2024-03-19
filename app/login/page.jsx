cdbbfaeaf902a560f68e99754aa7e4fffe050e46
RobinUrcun <107709383+RobinUrcun@users.noreply.github.com>
2024-03-19 01:37:23 +0100

    Systeme de connection : requete et recuperation des donnees

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,22 +1,57 @@
 "use client";
-import { useEffect } from "react";
 import React from "react";
 import Button from "../ui/Components/Button/Button";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function page() {
+  const [emailValue, setEmailValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
 
+  function getEmailValue(e) {
+    setEmailValue(e.target.value);
+  }
+  function getPasswordValue(e) {
+    setPasswordValue(e.target.value);
+  }
   return (
     <section className="loginSection">
       <form
         onSubmit={(e) => {
           e.preventDefault();
           e.stopPropagation();
+          fetch("http://localhost:3001/api/user/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              email: emailValue,
+              password: passwordValue,
+            }),
+          })
+            .then((response) =>
+              response.json().then((data) => {
+                localStorage.setItem("userInfoToken", data.token);
+                localStorage.setItem("userInfoUserId", data.userId);
+              })
+            )
+            .catch((error) => console.log(error));
         }}
       >
-        <input type="email" id="loginMail" placeholder="Adresse e-mail" />
+        <input
+          type="email"
+          id="loginMail"
+          placeholder="Adresse e-mail"
+          onChange={getEmailValue}
+        />
 
-        <input type="password" id="loginPassword" placeholder="Mot de passe" />
+        <input
+          type="password"
+          id="loginPassword"
+          placeholder="Mot de passe"
+          onChange={getPasswordValue}
+        />
 
         <Button type="submit">Se connecter</Button>
       </form>
