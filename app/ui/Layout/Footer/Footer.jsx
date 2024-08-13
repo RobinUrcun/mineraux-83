9ecb388463e3b7d62f23ba3f97b2090baa22eed2
RobinUrcun <107709383+RobinUrcun@users.noreply.github.com>
2024-08-13 19:05:28 +0200

    retrait des icones reseau / reponsive footer

--- app/ui/Layout/Footer/Footer.jsx
+++ app/ui/Layout/Footer/Footer.jsx
@@ -1,26 +1,8 @@
 import React from "react";
-import SocialCard from "./SocialCard/SocialCard";
 import Nav from "../../Components/Nav/Nav";
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footerSocial">
-        <SocialCard
-          href="www.facebook.com"
-          alt="visitez nous sur Facebook"
-          imgUrl="/assets/icone/social/facebook.webp"
-        />
-        <SocialCard
-          href="www.twitter.com"
-          alt="visitez nous sur Twitter"
-          imgUrl="/assets/icone/social/twitter.webp"
-        />
-        <SocialCard
-          href="www.instagram.com"
-          alt="visitez nous sur Instagram"
-          imgUrl="/assets/icone/social/instagram.webp"
-        />
-      </div>
       <Nav section="footer" />
     </footer>
   );
