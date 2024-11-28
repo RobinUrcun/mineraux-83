318fc5c3d8f78df4dac706cd0ddbb8da0e55b4ef
RobinUrcun <107709383+RobinUrcun@users.noreply.github.com>
2024-11-28 23:36:16 +0100

    balise image

--- app/ui/Pages/Boutique/Productcard/ProductCard.jsx
+++ app/ui/Pages/Boutique/Productcard/ProductCard.jsx
@@ -12,13 +12,10 @@ export default function ProductCard({ product }) {
   return (
     <Link href={`/produit/${product._id}`} className="productCard">
       <div className="productCardImgCont">
-        <Image
+        <img
           src={`https://lithosphere83-bucket.s3.eu-west-3.amazonaws.com/upload/${product.mainFile[0]}`}
           alt="ok"
           className="productCardImg"
-          width={200}
-          height={200}
-          priority={true}
         />
       </div>
       <div className="productCardInfo">
